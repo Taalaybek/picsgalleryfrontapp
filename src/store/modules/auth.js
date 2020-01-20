01675b5ebc8dfe6596867f990c84153be256cb3b
Taalay Eshen <taalaybekeshenov.96@gmail.com>
2020-01-21 02:09:10 +0600

    Implemented register action

--- src/store/modules/auth.js
+++ src/store/modules/auth.js
@@ -4,17 +4,34 @@ const auth = {
 	},
 
 	getters: {
-		checkAuth: state => {
-			return state.isAuthenticated;
-		}
+		checkAuth: state => state.isAuthenticated
 	},
 
 	mutations: {
-	
+		auth_success (state) {
+			state.isAuthenticated = true
+		},
+
+		auth_error (state) {
+			state.isAuthenticated = false
+		}
 	},
 
 	actions: {
-	
+		auth_register (context, data) {
+			context.commit('setRequestStatus', true)
+			return new Promise(function (resolve, reject) {
+				window.axios.post('auth/register', data)
+					.then(response => {
+						context.commit('setRequestStatus', false)
+						return resolve(response)
+					})
+					.catch(error => {
+						context.commit('setRequestStatus', false)
+						return reject(error)
+					})
+			})
+		}
 	}
 }
 
