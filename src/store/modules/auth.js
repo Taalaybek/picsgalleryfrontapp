0302b0a77540b0ac353866c9290d6fd19faa6a90
Taalay Eshen <taalaybekeshenov.96@gmail.com>
2020-01-21 12:04:22 +0600

    login action, auto detect cookies

--- src/store/modules/auth.js
+++ src/store/modules/auth.js
@@ -1,3 +1,5 @@
+import Vue from 'vue'
+
 const auth = {
 	state: {
 		isAuthenticated: false
@@ -14,6 +16,13 @@ const auth = {
 
 		auth_error (state) {
 			state.isAuthenticated = false
+		},
+		auth_detectViaCookies (state) {
+			if (Vue.$cookies.isKey('access_token') && Vue.$cookies.get('access_token')) {
+				state.isAuthenticated = true
+			}
+
+			state.isAuthenticated = true
 		}
 	},
 
@@ -30,6 +39,29 @@ const auth = {
 						context.commit('setRequestStatus', false)
 						return reject(error)
 					})
+			})
+		},
+
+		auth_login (context, data) {
+			context.commit('setRequestStatus', true)
+			return new Promise((resolve, reject) => {
+				window.axios.post('auth/login', data)
+				.then(response => {
+
+					context.commit('setRequestStatus', false)
+					Vue.$cookies.set('token_type', response.data.token_type)
+					Vue.$cookies.set('access_token', response.data.access_token)
+					Vue.$cookies.set('refresh_token', response.data.refresh_token)
+					Vue.$cookies.set('expires_in', response.data.expires_in)
+					Vue.$cookies.set('token', response.data.token_type+' '+response.data.access_token)
+					context.commit('auth_detectViaCookies')
+
+					return resolve(response)
+				})
+				.catch(error => {
+					context.commit('setRequestStatus', false)
+					return reject(error)
+				})
 			})
 		}
 	}
