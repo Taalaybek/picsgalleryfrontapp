1c131f3cab843bea13e689063254bb2ce7c1160b
Taalay Eshen <taalaybekeshenov.96@gmail.com>
2020-01-21 12:03:04 +0600

    Login page markup

---
+++ src/views/Login.vue
@@ -0,0 +1,83 @@
+<template>
+	<v-layout>
+		<v-row align="start" justify="center">
+			<v-col cols="12" sm="8" md="4">
+				<v-alert v-show="getGlobalMessage" type="info" class="white--text" transition="scale-transition" dense dismissible>{{ getGlobalMessage }}</v-alert>
+				<v-card elevation="5">
+					<v-progress-linear color="teal" :active="getRequestStatus" height="8" :indeterminate="getRequestStatus" bottom></v-progress-linear>
+
+					<v-toolbar color="primary" flat>
+						<v-toolbar-title class="white--text">Login form</v-toolbar-title>
+					</v-toolbar>
+
+					<v-card-text>
+						<v-form>
+							<v-text-field v-model="username" label="Username or Email" type="text" name="username" 
+							dense counter="60" append-outer-icon="mdi-account" v-validate="'required|min:6|max:60'" 
+							:error-messages="errors.has('username')?errors.first('username'):''" outlined required clearable></v-text-field>
+
+							<v-text-field v-model="password" label="Password" :type="showPassword?'text':'password'" 
+							name="password" :append-icon="showPassword?'mdi-eye-off':'mdi-eye'" dense counter="20" 
+							append-outer-icon="mdi-textbox-password" v-validate="'required|min:8|max:30'" 
+							:error-messages="errors.has('password')?errors.first('password'):''" @click:append="showPassword = !showPassword" 
+							outlined required clearable></v-text-field>
+						</v-form>
+					</v-card-text>
+
+					<v-card-actions>
+						<v-spacer />
+						<v-btn color="primary" :disabled="$validator.errors.any() || !this.isComplete" @click="submit" class="px-3">Login</v-btn>
+					</v-card-actions>
+				</v-card>
+			</v-col>
+		</v-row>
+	</v-layout>
+</template>
+<script>
+import { mapGetters, mapActions, mapMutations } from 'vuex'
+
+export default {
+	name: 'login',
+
+	data () {
+		return {
+			username: '',
+			password: '',
+			showPassword: false,
+			requestedStatus: true
+		}
+	},
+
+	computed: {
+		...mapGetters(['getRequestStatus', 'getGlobalMessage']),
+		isComplete() {
+			return this.username && this.password
+		}
+	},
+
+	methods: {
+		...mapActions(['auth_login']),
+		...mapMutations(['setGlobalMessage', 'cleanGlobalMessage']),
+		submit: function () {
+			const { username, password } = this
+			this.auth_login({ username, password })
+				.then(response => {
+					this.setGlobalMessage('Successfull login')
+					setTimeout(() => {
+						this.cleanGlobalMessage()
+						this.$router.push('/')
+					}, 3000);
+				})
+				.catch(error => {
+					if (error.response.status == 401) {
+						this.setGlobalMessage(error.response.data.message)
+					}
+
+					setTimeout(() => {
+						this.cleanGlobalMessage()
+					}, 5000); 
+				})
+		}
+	},
+}
+</script>
