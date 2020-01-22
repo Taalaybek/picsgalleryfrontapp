53c344acce8ad56b1f4a291ea6504ecb5a9a37c6
Taalay Eshen <taalaybekeshenov.96@gmail.com>
2020-01-22 13:07:55 +0600

    protected routes via middlewares

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,9 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import {auth} from '@/router/middlewares/auth'
+import {guest} from '@/router/middlewares/guest'
+import middlewarePipeline from './middlewarePipeline'
+import store from '@/store/index'
 
 Vue.use(VueRouter)
 
@@ -11,12 +15,32 @@ const routes = [
   {
     path: '/register',
     name: 'register',
-    component: ()=> import('@/views/Register')
+    component: ()=> import('@/views/Register'),
+    meta: {
+      middleware: [
+        guest
+      ]
+    }
   },
   {
     path: '/login',
     name: 'login',
-    component: ()=>import('@/views/Login')
+    component: ()=>import('@/views/Login'),
+    meta: {
+      middleware: [
+        guest
+      ]
+    }
+  },
+  {
+    path: '/home',
+    name: 'owner',
+    component: ()=>import('@/views/Home'),
+    meta: {
+      middleware: [
+        auth
+      ]
+    }
   }
 ]
 
@@ -24,6 +48,26 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  if (!to.meta.middleware) {
+    return next()
+  }
+
+  const middleware = to.meta.middleware
+
+  const context = {
+    to,
+    from,
+    next, 
+    store
+  }
+
+  return middleware[0]({
+    ...context,
+    next: middlewarePipeline(context, middleware, 1)
+  })
 })
 
 export default router
