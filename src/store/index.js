eee55a280ce3dd6a442d2a1ecd5d8424256cf00c
Taalay Eshen <taalaybekeshenov.96@gmail.com>
2020-01-22 13:33:39 +0600

    changed vuex-persistedState store format

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import createPersistedState from 'vuex-persistedstate'
+import Cookies from 'vue-cookies'
 
 Vue.use(Vuex)
 
@@ -32,5 +33,11 @@ export default new Vuex.Store({
   modules: {
     auth: auth
   },
-  plugins: [createPersistedState()]
+  plugins: [createPersistedState({
+    storage: {
+      getItem: key => Cookies.get(key),
+      setItem: (key, value) => Cookies.set(key, value, 60*60*24),
+      removeItem: key => Cookies.remove(key)
+    }
+  })]
 })
