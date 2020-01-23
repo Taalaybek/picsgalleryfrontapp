5dacb5a5a6072722a9ca889efa1f65564a8aa591
Taalay Eshen <taalaybekeshenov.96@gmail.com>
2020-01-23 16:31:45 +0600

    user vuex module with fetchUserData action and states

---
+++ src/store/modules/user.js
@@ -0,0 +1,55 @@
+import Vue from 'vue'
+import router from '@/router/index'
+
+const user = {
+	state: {
+		attributes: '',
+		user: ''
+	},
+
+	getters: {
+		getAttributes: state => state.attributes,
+		getUser: state => state.user
+	},
+
+	mutations: {
+		setAttributes(state, attributes) {
+			state.attributes = attributes
+		},
+		setUser(state, object) {
+			state.user = object
+		}
+	},
+
+	actions: {
+		fetchUserData(context) {
+			context.commit('overlayTrue')
+			return new Promise((resolve, reject) => {
+				window.axios.get('auth/user', { headers: { 'Authorization': `${Vue.$cookies.get('token_type')} ${Vue.$cookies.get('access_token')}`} })
+					.then(response => {
+						context.commit('overlayFalse')
+						context.commit('setUser', response.data)
+						context.commit('setAttributes', response.data.attributes)
+						return resolve(response)
+					})
+					.catch(error => {
+						context.commit('overlayFalse')
+						if (error.response.status == 401) {
+							context.commit('auth_error')
+							context.dispatch('auth_cleanCookies')
+							Vue.$notify.set({content: error.response.data.message, color: 'error'})
+							router.push('/login')
+						}
+
+						if (error.response.status == 500) {
+							Vue.$notify.set({content: 'Server shut down', color: 'error'})
+						}
+
+						return reject(error)
+					})
+			})
+		}
+	}
+}
+
+export default user
