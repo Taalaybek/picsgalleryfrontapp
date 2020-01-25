1ea60fe231ae6756d00f6f70a5378b926bcddcef
Taalay Eshen <taalaybekeshenov.96@gmail.com>
2020-01-25 16:41:26 +0600

    some messages and urls

---
+++ src/services/constants.js
@@ -0,0 +1,9 @@
+/*** MESSAGES ***/
+export const SUCCESS_LOGIN = 'Successfully login'
+export const SERVER_DOWN = 'Server not responding'
+export const UNDEFINED_ERROR = 'Undefined error on server'
+export const VALID_DATA = 'Please, enter valid data'
+export const INVALID_TOKEN = 'Invalid access token'
+
+export const API_URL = 'http://picsgallery.test:8080/api/v1'
+export const BASE_URL = 'http://localhost:8081'
